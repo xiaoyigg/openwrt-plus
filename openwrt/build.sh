4b702e249893b0e905989b6d4db06b63cf12910c
MapleCool <37971849+pmkol@users.noreply.github.com>
2024-10-13 20:57:31 +0800

    fix linux-rt support for aarch64/x86_64

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -388,6 +388,7 @@ fi
 # test kernel
 [ "$TESTING_KERNEL" = "y" ] && [ "$platform" = "bcm53xx" ] && sed -i '1i\# CONFIG_PACKAGE_kselftests-bpf is not set\n# CONFIG_PACKAGE_perf is not set\n' .config
 [ "$TESTING_KERNEL" = "y" ] && sed -i '1i\# Test kernel\nCONFIG_TESTING_KERNEL=y\n' .config
+[ "$TESTING_KERNEL" = "y" ] && [ "$version" = "rc2" ] && sed -i '/CONFIG_KERNEL_PREEMPT_RT/d' .config
 
 # not all kmod
 [ "$NO_KMOD" = "y" ] && sed -i '/CONFIG_ALL_KMODS=y/d; /CONFIG_ALL_NONSHARED=y/d' .config
